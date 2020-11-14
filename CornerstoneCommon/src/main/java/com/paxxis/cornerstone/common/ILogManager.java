72bec5d72144aebc66e8edb9a8b53d60a6bd3c4f
rob <rob@viserion.net>
2020-11-14 09:40:15 -0500

    Much_Much_Better

--- CornerstoneCommon/src/main/java/com/paxxis/cornerstone/common/ILogManager.java
+++
@@ -1,9 +0,0 @@
-package com.paxxis.cornerstone.common;
-
-
-public interface ILogManager {
-    public void setLogLevel(String level);
-    public String getLogLevel();
-    public void setAppender(String appender);
-    public String getAppender();
-}

903f304d91a4c54f0538d36f98d892cd87e0324b
rob <rob@viserion.net>
2020-11-14 09:13:44 -0500

    Much_Better

--- CornerstoneCommon/src/main/java/com/paxxis/cornerstone/common/ConfigurationResolver.java
+++
@@ -1,7 +0,0 @@
-package com.paxxis.cornerstone.common;
-
-import java.util.Collection;
-
-public interface ConfigurationResolver {
-    public Collection<String> getPrefixes();
-}
--- CornerstoneCommon/src/main/java/com/paxxis/cornerstone/common/PropertyConfigurable.java
+++
@@ -1,8 +0,0 @@
-package com.paxxis.cornerstone.common;
-
-import java.util.List;
-
-public interface PropertyConfigurable {
-    public void setValues(String propName, List<Object> values);
-    public void setValue(String propName, Object value);
-}
